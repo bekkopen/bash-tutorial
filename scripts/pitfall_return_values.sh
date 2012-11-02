ad2cace060b847f1fba7eb05491caa0586046e9f
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2012-11-02 10:01:27 +0100

    Simplified the pitfall_return_values.sh script

--- scripts/pitfall_return_values.sh
+++ scripts/pitfall_return_values.sh
@@ -10,21 +10,16 @@ while [ -h "$BASEDIR/$0" ]; do
 done
 cd ${BASEDIR}
 
-debug=true
-
 _combine_strings() {
   local string1=${1}
   local string2=${2}
 
   #echo "debug: string1 is ${string1} and string2 is ${string2}"
-  [[ debug ]] && debugarr==( "debug: string1 is ${string1} and string2 is ${string2}" )
-  #echo "${debug_combine_strings}"
 
   echo ${string1}${string2}
 }
 
 combined_strings=$( _combine_strings "Hello " "World" )
 
-[[ debug ]] && echo "${debugarr[@]}"
 echo "The combined string is: ${combined_strings}"
 

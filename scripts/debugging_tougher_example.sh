3eb251ce5b0977f8664a57a163298d42cc03aa0b
Stein Inge Morisbak <steinim@gmail.com>
2013-03-13 09:41:09 +0100

    fixed small bug

--- scripts/debugging_tougher_example.sh
+++ scripts/debugging_tougher_example.sh
@@ -9,14 +9,14 @@ if [ "${version}" != "rollback" ] && [ ${#artifactsAndVersions[@]} -ne 0 ]; then
       if ( _is_snapshot ${deployVersion} ); then
         if [ -s ~/deploy-${deployVersion}.zip ]; then
           _debug "Found deploy-${deployVersion}.zip in homedir. Moving it to ${deployFile}."
-        _run "echo ~/deploy-${deployVersion}.zip ${deployFile}"
+          _run "echo ~/deploy-${deployVersion}.zip ${deployFile}"
         if [ ! -s ~/deploy-${deployVersion}.zip ]; then
           _fatal "You are trying to deploy a SNAPSHOT version, but it is not installed"
         fi
       else
         _fatal "Could not find deploy-${deployVersion}.zip at ${deployFile}."
       fi
-      [[ -e ${deployFile} ]] || _fatal "${deployFile} not found locally or in Nexus! Exiting!"
+      [ -e ${deployFile} ] || _fatal "${deployFile} not found locally or in Nexus! Exiting!"
     fi
     artifactFilesToUpload+=( "${deployFile}" )
   else
